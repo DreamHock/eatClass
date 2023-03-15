72ab2382fcddc54bffe78ae98d6c08f013333ba1
yahya harakat <81807660+DreamHock@users.noreply.github.com>
2023-03-15 12:49:01 +0000

    improve logic and fix bugs

---
+++ resources/js/Components/AddDay.jsx
@@ -0,0 +1,149 @@
+import { router } from "@inertiajs/react";
+import { useState } from "react";
+
+const AddDay = () => {
+    const [day, setDay] = useState("day");
+    const [services, setServices] = useState([{ id: 1, service: "" }]);
+
+    const addService = () => {
+        const newService = {
+            id: services[services.length - 1].id + 1,
+            service: "",
+        };
+        setServices([...services, newService]);
+    };
+
+    const handleServiceChange = (service, id) => {
+        let desiredService = services.map((s) => {
+            if (s.id === id) {
+                s.service = service;
+            }
+            return s
+        });
+        setServices(desiredService);
+    };
+
+    const deleteService = (id) => {
+        services.length > 1
+            ? setServices(
+                  services.filter((service) => {
+                      return service.id != id;
+                  })
+              )
+            : null;
+    };
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        router.post("/defaultServices", {
+            idRestaurant: 1,
+            day,
+            services,
+        });
+    }
+
+    return (
+        <form
+            onSubmit={(e) => {
+                handleSubmit(e);
+            }}
+            className="shadow-md p-4 w-52 flex flex-col"
+        >
+            <select
+                className="p-1 h-8 rounded"
+                onChange={(e) => setDay(e.target.value)}
+                value={day}
+            >
+                <option value="day" disabled>
+                    Day
+                </option>
+                <option value="monday">Monday</option>
+                <option value="monday">Tuesday</option>
+                <option value="monday">Wednesday</option>
+                <option value="thursday">Thursday</option>
+                <option value="friday">Friday</option>
+                <option value="saturday">Saturday</option>
+                <option value="sunday">Sunday</option>
+            </select>
+            <div>sevices:</div>
+            {services.map((ser) => {
+                return (
+                    <ServiceInput
+                        ser={ser}
+                        key={ser.id}
+                        addService={addService}
+                        deleteService={deleteService}
+                        handleServiceChange={handleServiceChange}
+                    />
+                );
+            })}
+
+            <div
+                onClick={() => {
+                    addService();
+                }}
+                className={`border border-slate-900 rounded h-8 shadow-sm p-1 flex items-center overflow-hidden w-full mb-2 text-slate-400 text-2xl justify-end px-1"
+                            `}
+            >
+                <div className="select-none">+</div>
+            </div>
+            <button
+                type="submit"
+                className="focus:outline-none text-white bg-green-500 hover:bg-green-600 font-medium rounded text-sm px-5 py-2.5"
+            >
+                Add day
+            </button>
+        </form>
+    );
+};
+
+export default AddDay;
+
+const ServiceInput = ({ ser, deleteService, handleServiceChange }) => {
+    const [inputActive, setInputActive] = useState(true);
+    const [service, setService] = useState("");
+
+    return (
+        <div>
+            <div className="mb-2 flex relative">
+                {inputActive ? (
+                    <input
+                        onChange={(e) => {
+                            setService(e.target.value);
+                            handleServiceChange(e.target.value, ser.id);
+                        }}
+                        value={service}
+                        autoFocus
+                        onBlur={() => {
+                            setInputActive(false);
+                        }}
+                        // onFocus={}
+                        type="text"
+                        placeholder="service"
+                        className="border border-slate-900 rounded h-8 p-1 flex justify-end items-center shadow-sm w-full"
+                    />
+                ) : (
+                    <div
+                        onClick={() => {
+                            setInputActive(true);
+                        }}
+                        className={`border border-slate-900 rounded h-8 shadow-sm p-1 flex items-center overflow-hidden w-full ${
+                            !service &&
+                            "text-slate-400 text-2xl justify-end px-1"
+                        }`}
+                    >
+                        {service}
+                    </div>
+                )}
+                <div
+                    onClick={() => {
+                        deleteService(ser.id);
+                    }}
+                    className="flex justify-center items-center absolute right-[-11px] top-1 select-none text-red-600 font-extrabold cursor-pointer"
+                >
+                    x
+                </div>
+            </div>
+        </div>
+    );
+};
